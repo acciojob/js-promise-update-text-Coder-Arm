bf9d836c4523bd5b01db1ad60a2ef0f5d5ef72eb
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-06-22 13:51:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+let prom = new Promise((resolve) => {
+	setTimeout(() => {
+		let str = "Hello, world!";
+		resolve(str);
+	},1000);
+});
+
+prom.then((str) => {
+	output.innerText = str;
+});
